46cb356769087dee019a3384ee914a01e74d24b5
Leucioiu Stefan <leucioiu.stefan@yahoo.com>
2021-01-06 18:24:23 +0200

    Refactored sandwich ingredients logic

--- src/components/Sandwich/Sandwich.jsx
+++ src/components/Sandwich/Sandwich.jsx
@@ -3,18 +3,17 @@ import SandwichIngredient from './SandwichIngredient/SandwichIngredient';
 import { sandwich } from './sandwichClasses';
 
 const Sandwich = ({ ingredients }) => {
-  let transformedIngredients = Object.keys(ingredients)
-    .map((ingKey) =>
-      [...Array(ingredients[ingKey])].map((_, index) => (
-        <SandwichIngredient key={ingKey + index} type={ingKey} />
-      )),
-    )
-    .reduce((arr, el) => {
-      return arr.concat(el);
-    }, []);
-
+  let transformedIngredients = [];
+  for (let ingredient in ingredients) {
+    let quantity = ingredients[ingredient];
+    for (let i = 0; i < quantity; i++) {
+      transformedIngredients.push(
+        <SandwichIngredient key={ingredient + i} type={ingredient} />,
+      );
+    }
+  }
   if (transformedIngredients.length === 0) {
-    transformedIngredients = <p>Please add ingredients</p>;
+    transformedIngredients = <p>Please start adding ingredients!</p>;
   }
 
   return (
